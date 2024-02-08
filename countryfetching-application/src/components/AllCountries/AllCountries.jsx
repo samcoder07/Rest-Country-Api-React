e98a263664f3309a845982a817078ee1090e7183
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2024-02-08 23:43:42 +0530

    checkin

---
+++ countryfetching-application/src/components/AllCountries/AllCountries.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AllCountries = () => {
+  return (
+    <div className="all_country_wrapper">
+      <div className="country_top"></div>
+      <div className="country_bottom"></div>
+    </div>
+  );
+};
+
+export default AllCountries;
